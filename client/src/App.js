7e0b32eda6d7204e596170ce5f36982518499b8d
Zimam-Ahmed <zimamahmad48@gmail.com>
2024-07-29 17:11:38 +0500

    react app created

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main className='font-bold text-2xl text-red-600'> 
+      React App
+     </main>
+  );
+}
+
+export default App;
