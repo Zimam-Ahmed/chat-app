b280373a1fca97a26188eea855e91ab7a8a35959
Zimam-Ahmed <zimamahmad48@gmail.com>
2024-08-05 16:17:56 +0500

    Cloudinary Integrated

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { IoClose, IoGitNetworkOutline } from "react-icons/io5";
+import { Link } from 'react-router-dom';
+import uploadFiles from '../helpers/uploadFiles';
 
 const RegisterPage = () => {
   const [data, setData] = useState({
@@ -20,20 +22,37 @@ const handleOnChange = (e) =>{
   })
 }
 
-const handleUploadPhoto = (e) =>{
+const handleUploadPhoto = async(e) =>{
   const file = e.target.files[0];
+  const uploadPhoto = await uploadFiles(file);
   setUploadPhoto(file)
+
+  setData((preve)=>{
+    return{
+      ...preve,
+      profile_pic : uploadPhoto.url
+    }
+  })
 }
 
-const handlClearUploadPhoto = () =>{
+const handlClearUploadPhoto = (e) =>{
+  e.stopPropagation()
+  e.preventDefault()
   setUploadPhoto(null)
 }
 
+const handleSubmit = (e) =>{
+  e.preventDefault()
+  e.stopPropagation()
+
+  const url = `${proccess.en}/api/register`
+}
+
   return (
-    <div className='mt-5'>
-      <div className='bg-white w-full max-w-sm mx-2 rounded overflow-hidden p-4'>
+    <div className='mt-14'>
+      <div className='bg-white w-full max-w-md rounded mx:2 overflow-hidden p-4 mx-auto md:mx-auto'>
         <h3>Welcome to Chat App</h3>
-        <form className='grid gap-3 mt-5'>
+        <form className='grid gap-3 mt-5' onSubmit={handleSubmit}>
           <div className='flex flex-col gap-1'>
             <label htmlFor='name'>Name : </label>
             <input
@@ -94,8 +113,10 @@ const handlClearUploadPhoto = () =>{
               className='bg-slate-100 px-2 pt-1 focus:outline-primary hidden'
               onChange={handleUploadPhoto}
             />
+            <button className='bg-primary text-lg px-4 py-1 hover:bg-secondary rounded mt-4 font-bold text-white leading-relaxed tracking-wide'>Register</button>
           </div>
         </form>
+        <p className='my-3 text-center'> Already Have A Account ? <Link to={'/email'} className='hover:text-primary font-semibold'>Login</Link></p>
       </div>
     </div>
   )
